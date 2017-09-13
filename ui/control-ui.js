16c47ffa820ba0ca4911a53f7d145437fd176a0a
Staffan Runnsjö <staffan.runnsjo@gmail.com>
2017-09-13 14:57:49 +0200

    Move javascript from index.html
    Also introduce a click state function to reduce the likelyhood of
    double clicks and make the control button behaviour the same
    everywhere.

---
+++ ui/control-ui.js
@@ -0,0 +1,65 @@
+const dataStorage = require('../file_io/datastorage.js');
+const ui = require('./table-ui.js');
+
+
+function delayButtonRepeatClick(button, clickFunction, event) {
+	let disabledState = button.prop('disabled');
+	button.prop('disabled', 'disabled');
+	button.off('click');
+	window.setTimeout(function() {
+		button.click(clickFunction);
+		button.prop('disabled', disabledState);
+	}, 1000);
+	event.stopPropagation();
+}
+
+function loadDirectory(directory) {
+	$('#renderArea').html('<div class="container"><p>Loading phrases from ' + directory + '</p></div>');
+	dataStorage.load(directory);
+	$('button').prop("disabled", false);
+	ui.renderData();
+}
+
+function initateControls() {
+	let loadData = function(event) {
+		delayButtonRepeatClick($(this), loadData, event);
+		let dialog = require('electron').remote.dialog;
+		let fileNames = dialog.showOpenDialog({properties: ['openDirectory']});
+		if(fileNames && fileNames.length) {
+			loadDirectory(fileNames[0]);
+			settingsHandler.update({lastOpenDirectory : fileNames[0]}, 'user');
+		};
+	};
+	$('#loadData').click(loadData);
+
+	let saveData = function(event) {
+		delayButtonRepeatClick($(this), saveData, event);
+		if(dataStorage.isDirty()) {
+			dataStorage.save();
+			ui.renderData();
+		}
+	};
+	$('#saveData').click(saveData);
+
+	let reloadData = function(event) {
+		delayButtonRepeatClick($(this), reloadData, event);
+		dataStorage.load();
+		ui.renderData();
+	}
+	$('#reloadData').click(reloadData);
+
+	let createNew = function(event) {
+		delayButtonRepeatClick($(this), createNew, event);
+		ui.addRow();
+	};
+	$('#createNew').click(createNew);
+
+	let searchTimerId;
+	$('input.search').keyup(function() {
+		clearTimeout(searchTimerId);
+		searchTimerId = window.setTimeout(ui.filterForSearch, 300);
+	});
+}
+
+module.exports.loadDirectory = loadDirectory;
+module.exports.initateControls = initateControls;
