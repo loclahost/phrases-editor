f46ba53d98824983ab3b98213e987d37bee25fde
Staffan Runnsjö <staffan.runnsjo@gmail.com>
2017-10-02 21:37:47 +0200

    Added watch for file content changes. The editor will now warn if the contents of the opened phrases files have changed on disk compared to the ones loaded.
    Fixes #9

--- file_io/filehandler.js
+++ file_io/filehandler.js
@@ -1,10 +1,15 @@
+const watch = require('node-watch');
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
 const javaEnumFactory = require('./java-enum-factory.js');
 const settingsHandler = require('../settings/settings-handler.js');
+const notificationUI = require('../ui/notification-ui.js');
+
+let fileWatch;
+let notificationId;
 
 function loadData(directory) {
-	let path = require('path');
-	let fs = require('fs');
-
 	console.log('Loading files from ' + directory);
 
 	let phrasesData = {
@@ -16,18 +21,27 @@ function loadData(directory) {
 		return phrasesData;
 	}
 
+	if(fileWatch) {
+		fileWatch.close();
+	}
+
+	notificationUI.clearNotifications();
+
 	let files = fs.readdirSync(directory);
 
 	files = files.filter((element) => element.endsWith('.phrases'));
 
 	let contentMap = {};
 	for(let index = 0; index < files.length; index++) {
-		phrasesData.meta.push({
+		let meta = {
 			name : files[index],
 			path : path.resolve(directory, files[index])
-		});
+		};
+
+		phrasesData.meta.push(meta);
 
 		let content = fs.readFileSync(phrasesData.meta[index].path, 'utf8');
+		meta.md5 = createMD5(content);
 		content = content.replace('\r', '');
 		let lines = content.split('\n');
 		for(let i = 0; i < lines.length; i++) {
@@ -46,14 +60,40 @@ function loadData(directory) {
 		});
 	}
 
+
+	fileWatch = watch(directory, { recursive: true }, function(event, filename) {
+		if(filename.endsWith('.phrases') && path.dirname(filename) == directory) {
+			let changedMeta;
+			for(let i = 0; i < phrasesData.meta.length; i++) {
+				if(phrasesData.meta[i].path == filename) {
+					changedMeta = phrasesData.meta[i];
+				}
+			}
+
+			let originalMD5 = changedMeta.md5;
+			if(originalMD5) {
+				fs.readFile(filename, 'utf8', function(err, data) {
+					if(err) {
+						throw err;
+					}
+
+					if(changedMeta.notificationId) {
+						notificationUI.removeNotification(changedMeta.notificationId);
+					}
+
+					if(createMD5(data) != originalMD5) {
+						changedMeta.notificationId = notificationUI.createNotification("The file '" + filename + "' has changed on disk", 'warning');
+					}
+				});
+			}
+		}
+	});
+
 	return phrasesData;
 }
 
 function saveData(phrases, directory) {
 	console.log("Saving data");
-
-	let path = require('path');
-	let fs = require('fs');
 
 	let meta = phrases.getMeta();
 	let content = phrases.getContent();
@@ -65,7 +105,9 @@ function saveData(phrases, directory) {
 	let fileContent = content.map((element) => element.content);
 
 	for(let i = 0; i < meta.length; i++) {
-		fs.writeFile(meta[i].path, createPhrasesFileContents(fileContent, i + 1), function() {console.log('Done writing ' + meta[i].path)});
+		let newContent = createPhrasesFileContents(fileContent, i + 1);
+		meta[i].md5 = createMD5(newContent);
+		fs.writeFile(meta[i].path, newContent, function() {console.log('Done writing ' + meta[i].path)});
 	}
 
 	settingsHandler.get().then(settings => {
@@ -87,6 +129,10 @@ function saveData(phrases, directory) {
 	}
 
 	return content;
+}
+
+function createMD5(fileContent) {
+	return crypto.createHash('md5').update(fileContent).digest("hex");
 }
 
 module.exports.loadData = loadData;
---
+++ ui/notification-ui.js
@@ -0,0 +1,46 @@
+const uuidv4 = require('uuid/v4');
+
+let notifications = {};
+
+function createNotification(notification, type) {
+	let notificationId = uuidv4();
+	notifications[notificationId] = {
+		timestamp: Date.now(),
+		data: notification,
+		type: type || 'info'
+	};
+
+	renderNotifications();
+
+	return notificationId;
+}
+
+function removeNotification(notificationId) {
+	delete notifications[notificationId];
+
+	renderNotifications();
+}
+
+function clearNotifications() {
+	notifications = {};
+
+	renderNotifications();
+}
+
+function renderNotifications() {
+	let orderedNotifications = [];
+	for(let key in notifications) {
+		orderedNotifications.push(notifications[key]);
+	}
+
+	let notificationArea = $('#notification-area');
+	notificationArea.empty();
+
+	orderedNotifications
+	.sort((a,b) => a.timestamp - b.timestamp)
+	.forEach((element) => notificationArea.append('<div class="notification notification-type-' + element.type + '">' + element.data + '</div>'));
+}
+
+module.exports.createNotification = createNotification;
+module.exports.removeNotification = removeNotification;
+module.exports.clearNotifications = clearNotifications;
