8d471fea80aa1d3c42a69446bbfe1795d46ab6e4
Fredrik Moos <fredrik.moos@netset.se>
2017-10-09 09:52:49 +0200

    Add support for creating new windows on OSX
    If the process is already running, create new window if clicking the icon.

--- main.js
+++ main.js
@@ -2,58 +2,64 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
-
-
 let mainWindow;
 
-app.on('ready', function() {
-	mainWindow = new BrowserWindow({ width: 1200, height: 600 });
+function createNewWindow() {
+	mainWindow = new BrowserWindow({width: 1200, height: 600});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-	mainWindow.on('closed', function() {
+	mainWindow.on('closed', function () {
 		mainWindow = null;
 	});
 
-	var template = [{
-			label: "Application",
-			submenu: [
-				{ label: 'Toggle Developer Tools', accelerator: 'Alt+Command+I', click: function() { mainWindow.toggleDevTools(); } },
-				{
-					label: "Save",
-					accelerator: "CmdOrCtrl+S",
-					selector: "save:",
-					click: function() {
-						mainWindow.webContents.send('file-save', 'save-editor');
-					}
-				},
-				{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); } }
-			]
+	var template = [
+	{
+		label: "Application",
+		submenu: [
+		{
+			label: 'Toggle Developer Tools',
+			accelerator: 'Alt+Command+I',
+			click: function() { mainWindow.toggleDevTools(); }
 		},
 		{
-			label: "Edit",
-			submenu: [
-				{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-				{ type: "separator" },
-				{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-				{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-				{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-				{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-			]
-		}
+			label: "Save",
+			accelerator: "CmdOrCtrl+S",
+			selector: "save:",
+			click: function() {
+				mainWindow.webContents.send('file-save', 'save-editor');
+			}
+		},
+		{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+		]
+	}, {
+		label: "Edit",
+		submenu: [
+		{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+		{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+		{ type: "separator" },
+		{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+		{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+		{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+		{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]
+	}
 	];
 
 	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+app.on('ready', function() {
+	createNewWindow();
 });
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
 	if (mainWindow === null) {
-		createWindow();
+		createNewWindow();
 	}
 });
