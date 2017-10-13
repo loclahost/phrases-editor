03a6e5b635ab9b0bbfeef439bc830994118d8653
Fredrik Moos <fredrik.moos@netset.se>
2017-10-13 10:36:11 +0200

    macOS For for window management
    Fixed support for windows that are closed but application not quit.
    
    Added native support for hiding windows on macOS.
    
    Added package lock file to repo.

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function createNewWindow() {
 					mainWindow.webContents.send('file-save', 'save-editor');
 				}
 			},
-			{ label: "Hide", accelerator: "Command+H", click: function() { mainWindow.hide(); } },
+			{ label: "Hide", accelerator: "Command+H", click: function() { app.hide(); } },
 			{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); } }
 		]
 	}, {
@@ -59,5 +59,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
 	if (mainWindow === null) {
 		createNewWindow();
+	} else {
+		app.show();
 	}
 });
